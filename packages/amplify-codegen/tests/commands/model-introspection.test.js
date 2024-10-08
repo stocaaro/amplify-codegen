6e0dfaa73fa7c1ac7883b7dabf6bb776351098ea
Aaron S <stocaaro@stocad.com>
2024-10-08 14:00:19 -0500

    Reduce diff size a bit

--- packages/amplify-codegen/tests/commands/model-introspection.test.js
+++ packages/amplify-codegen/tests/commands/model-introspection.test.js
@@ -21,12 +21,12 @@ jest.mock('@graphql-codegen/core', () => {
   };
 });
 
-const MOCK_GENERATED_INTROSPECTION = { schemaVersion: 1 };
-const MOCK_GENERATED_CODE = JSON.stringify(MOCK_GENERATED_INTROSPECTION);
 const MOCK_OUTPUT_DIR = 'output';
 const MOCK_PROJECT_ROOT = 'project';
 const MOCK_PROJECT_NAME = 'myapp';
 const MOCK_BACKEND_DIRECTORY = 'backend';
+const MOCK_GENERATED_INTROSPECTION = { schemaVersion: 1 };
+const MOCK_GENERATED_CODE = JSON.stringify(MOCK_GENERATED_INTROSPECTION);
 
 const MOCK_CONTEXT = {
   print: {
@@ -55,91 +55,87 @@ const MOCK_CONTEXT = {
   parameters: {},
 };
 
-describe('model-introspection', () => {
+describe('generateModelIntrospection', () => {
+  const schemaFilePath = path.join(MOCK_BACKEND_DIRECTORY, 'api', MOCK_PROJECT_NAME);
+  const outputDirectory = path.join(MOCK_PROJECT_ROOT, MOCK_OUTPUT_DIR);
+  const mockedFiles = {};
+  mockedFiles[schemaFilePath] = {
+    'schema.graphql': ' type SimpleModel { id: ID! status: String } ',
+  };
+  mockedFiles[outputDirectory] = {};
+
   beforeAll(() => {
     codegen_core.codegen.mockReturnValue(MOCK_GENERATED_CODE);
-  })
-
-  describe('generateModelIntrospection', () => {
-    const schemaFilePath = path.join(MOCK_BACKEND_DIRECTORY, 'api', MOCK_PROJECT_NAME);
-    const outputDirectory = path.join(MOCK_PROJECT_ROOT, MOCK_OUTPUT_DIR);
-    const mockedFiles = {};
-    mockedFiles[schemaFilePath] = {
-      'schema.graphql': ' type SimpleModel { id: ID! status: String } ',
-    };
-    mockedFiles[outputDirectory] = {};
-
-    beforeAll(() => {
-      graphqlGenerator.generateModels.mockReturnValue({ 'model-introspection.json': MOCK_GENERATED_CODE });
-    });
-
-    beforeEach(() => {
-      jest.clearAllMocks();
-    });
-
-    it('should generate model intropection schema', async () => {
-      mockFs(mockedFiles);
-      // assert empty folder before generation
-      expect(fs.readdirSync(outputDirectory).length).toEqual(0);
-      const contextWithOutputDir = {
-        ...MOCK_CONTEXT,
-        parameters: {
-          options: {
-            'output-dir': MOCK_OUTPUT_DIR,
-          },
-        },
-      };
-      await expect(generateModelIntrospection(contextWithOutputDir)).resolves.not.toThrow();
-      // assert model generation succeeds with a single schema file
-      expect(graphqlGenerator.generateModels).toBeCalled();
-      expect(fs.readdirSync(outputDirectory).length).toBeGreaterThan(0);
-    });
-    it('should throw error if the output dir is not included in the command', async () => {
-      mockFs(mockedFiles);
-      // assert empty folder before generation
-      expect(fs.readdirSync(outputDirectory).length).toEqual(0);
-      await expect(generateModelIntrospection(MOCK_CONTEXT)).rejects.toThrowError();
-      // assert model generation failure with no file found
-      expect(graphqlGenerator.generateModels).not.toBeCalled();
-      expect(fs.readdirSync(outputDirectory).length).toEqual(0);
-    });
-
-    afterEach(mockFs.restore);
+    graphqlGenerator.generateModels.mockReturnValue({ 'model-introspection.json': MOCK_GENERATED_CODE });
   });
 
-  describe('getModelIntrospection', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    const schemaFilePath = path.join(MOCK_BACKEND_DIRECTORY, 'api', MOCK_PROJECT_NAME);
-    const outputDirectory = path.join(MOCK_PROJECT_ROOT, MOCK_OUTPUT_DIR);
-    const mockedFiles = {};
-    mockedFiles[schemaFilePath] = {
-      'schema.graphql': ' type SimpleModel { id: ID! status: String } ',
+  it('should generate model intropection schema', async () => {
+    mockFs(mockedFiles);
+    // assert empty folder before generation
+    expect(fs.readdirSync(outputDirectory).length).toEqual(0);
+    const contextWithOutputDir = {
+      ...MOCK_CONTEXT,
+      parameters: {
+        options: {
+          'output-dir': MOCK_OUTPUT_DIR,
+        },
+      },
     };
-    mockedFiles[outputDirectory] = {};
+    await expect(generateModelIntrospection(contextWithOutputDir)).resolves.not.toThrow();
+    // assert model generation succeeds with a single schema file
+    expect(graphqlGenerator.generateModels).toBeCalled();
+    expect(fs.readdirSync(outputDirectory).length).toBeGreaterThan(0);
+  });
+  it('should throw error if the output dir is not included in the command', async () => {
+    mockFs(mockedFiles);
+    // assert empty folder before generation
+    expect(fs.readdirSync(outputDirectory).length).toEqual(0);
+    await expect(generateModelIntrospection(MOCK_CONTEXT)).rejects.toThrowError();
+    // assert model generation failure with no file found
+    expect(graphqlGenerator.generateModels).not.toBeCalled();
+    expect(fs.readdirSync(outputDirectory).length).toEqual(0);
+  });
 
-    beforeAll(() => {
-      graphqlGenerator.generateModels.mockReturnValue({ 'model-introspection.json': MOCK_GENERATED_CODE });
-    });
+  afterEach(mockFs.restore);
+});
 
-    beforeEach(() => {
-      jest.clearAllMocks();
-    });
+describe('getModelIntrospection', () => {
 
-    it('should return model intropection schema', async () => {
-      mockFs(mockedFiles);
-      // assert empty folder before generation
-      expect(fs.readdirSync(outputDirectory).length).toEqual(0);
+  const schemaFilePath = path.join(MOCK_BACKEND_DIRECTORY, 'api', MOCK_PROJECT_NAME);
+  const outputDirectory = path.join(MOCK_PROJECT_ROOT, MOCK_OUTPUT_DIR);
+  const mockedFiles = {};
+  mockedFiles[schemaFilePath] = {
+    'schema.graphql': ' type SimpleModel { id: ID! status: String } ',
+  };
+  mockedFiles[outputDirectory] = {};
 
-      const responseObject = await getModelIntrospection(MOCK_CONTEXT);
-      expect(responseObject).toEqual(MOCK_GENERATED_INTROSPECTION);
+  beforeAll(() => {
+    codegen_core.codegen.mockReturnValue(MOCK_GENERATED_CODE);
+    graphqlGenerator.generateModels.mockReturnValue({ 'model-introspection.json': MOCK_GENERATED_CODE });
+  });
 
-      // assert model generation succeeds with no file written
-      expect(graphqlGenerator.generateModels).toBeCalled();
-      expect(fs.readdirSync(outputDirectory).length).toEqual(0);
-    });
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    afterEach(() => {
-      mockFs.restore();
-    });
+  it('should return model intropection schema', async () => {
+    mockFs(mockedFiles);
+    // assert empty folder before generation
+    expect(fs.readdirSync(outputDirectory).length).toEqual(0);
+
+    const responseObject = await getModelIntrospection(MOCK_CONTEXT);
+    expect(responseObject).toEqual(MOCK_GENERATED_INTROSPECTION);
+
+    // assert model generation succeeds with no file written
+    expect(graphqlGenerator.generateModels).toBeCalled();
+    expect(fs.readdirSync(outputDirectory).length).toEqual(0);
+  });
+
+  afterEach(() => {
+    mockFs.restore();
   });
 });
